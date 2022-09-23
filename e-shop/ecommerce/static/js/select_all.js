b3b9542d83eb8a55bbee300f69ac1dd00af5fc97
Alex <xllxlex@gmail.com>
2022-09-23 22:22:18 +0200

    changes

---
+++ e-shop/ecommerce/static/js/select_all.js
@@ -0,0 +1,10 @@
+function selects(){
+                var ele=document.getElementsByName('check');
+                for(var i=0; i<ele.length; i++){
+                    if(ele[i].type=='checkbox')
+                        ele[i].checked=true;
+                }
+            }
+
+let div = document.getElementsByName
+
